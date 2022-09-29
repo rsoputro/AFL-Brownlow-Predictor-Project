feba0425bd21faa82881eb8418c55a02efe08695
Luke-Mason <lukeymason.15@gmail.com>
2022-09-29 18:29:38 +1000

    Added initial files and R notebook

---
+++ MoreUtilities.R
@@ -0,0 +1,161 @@
+#===============PRELIMINARY FUNCTIONS FOR POSTERIOR INFERENCES====================
+
+smryMCMC = function(  codaSamples , compVal = NULL,  saveName=NULL) {
+  summaryInfo = NULL
+  mcmcMat = as.matrix(codaSamples,chains=TRUE)
+  paramName = colnames(mcmcMat)
+  for ( pName in paramName ) {
+    if (pName %in% colnames(compVal)){
+      if (!is.na(compVal[pName])) {
+        summaryInfo = rbind( summaryInfo , summarizePost( paramSampleVec = mcmcMat[,pName] , 
+                                                          compVal = as.numeric(compVal[pName]) ))
+      }
+      else {
+        summaryInfo = rbind( summaryInfo , summarizePost( paramSampleVec = mcmcMat[,pName] ) )
+      }
+    } else {
+      summaryInfo = rbind( summaryInfo , summarizePost( paramSampleVec = mcmcMat[,pName] ) )
+    }
+  }
+  rownames(summaryInfo) = paramName
+  
+  # summaryInfo = rbind( summaryInfo , 
+  #                      "tau" = summarizePost( mcmcMat[,"tau"] ) )
+  if ( !is.null(saveName) ) {
+    write.csv( summaryInfo , file=paste(saveName,"SummaryInfo.csv",sep="") )
+  }
+  return( summaryInfo )
+}
+
+#===============================================================================
+
+plotMCMC_HD = function( codaSamples , data , xName="x" , yName="y", preds = FALSE ,
+                        showCurve=FALSE ,  pairsPlot=FALSE , compVal = NULL, 
+                        saveName=NULL , saveType="jpg" ) {
+  # showCurve is TRUE or FALSE and indicates whether the posterior should
+  #   be displayed as a histogram (by default) or by an approximate curve.
+  # pairsPlot is TRUE or FALSE and indicates whether scatterplots of pairs
+  #   of parameters should be displayed.
+  #-----------------------------------------------------------------------------
+  y = data[,yName]
+  x = as.matrix(data[,xName])
+  mcmcMat = as.matrix(codaSamples,chains=TRUE)
+  chainLength = NROW( mcmcMat )
+  # zbeta0 = mcmcMat[,"zbeta0"]
+  # zbeta  = mcmcMat[,grep("^zbeta$|^zbeta\\[",colnames(mcmcMat))]
+  # if ( ncol(x)==1 ) { zbeta = matrix( zbeta , ncol=1 ) }
+  beta0 = mcmcMat[,"beta0"]
+  beta  = mcmcMat[,grep("^beta$|^beta\\[",colnames(mcmcMat))]
+  if ( ncol(x)==1 ) { beta = matrix( beta , ncol=1 ) }
+  if (preds){
+    pred = mcmcMat[,grep("^pred$|^pred\\[",colnames(mcmcMat))]
+  } # Added by Demirhan
+  #-----------------------------------------------------------------------------
+  # Compute R^2 for credible parameters:
+  YcorX = cor( y , x ) # correlation of y with each x predictor
+  Rsq = beta %*% matrix( YcorX , ncol=1 )
+  #-----------------------------------------------------------------------------
+  if ( pairsPlot ) {
+    # Plot the parameters pairwise, to see correlations:
+    openGraph()
+    nPtToPlot = 1000
+    plotIdx = floor(seq(1,chainLength,by=chainLength/nPtToPlot))
+    panel.cor = function(x, y, digits=2, prefix="", cex.cor, ...) {
+      usr = par("usr"); on.exit(par(usr))
+      par(usr = c(0, 1, 0, 1))
+      r = (cor(x, y))
+      txt = format(c(r, 0.123456789), digits=digits)[1]
+      txt = paste(prefix, txt, sep="")
+      if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+      text(0.5, 0.5, txt, cex=1.25 ) # was cex=cex.cor*r
+    }
+    pairs( cbind( beta0 , beta , tau )[plotIdx,] ,
+           labels=c( "beta[0]" , 
+                     paste0("beta[",1:ncol(beta),"]\n",xName) , 
+                     expression(tau) ) , 
+           lower.panel=panel.cor , col="skyblue" )
+    if ( !is.null(saveName) ) {
+      saveGraph( file=paste(saveName,"PostPairs",sep=""), type=saveType)
+    }
+  }
+  #-----------------------------------------------------------------------------
+  # Marginal histograms:
+  
+  decideOpenGraph = function( panelCount , saveName , finished=FALSE , 
+                              nRow=2 , nCol=3 ) {
+    # If finishing a set:
+    if ( finished==TRUE ) {
+      if ( !is.null(saveName) ) {
+        saveGraph( file=paste0(saveName,ceiling((panelCount-1)/(nRow*nCol))), 
+                   type=saveType)
+      }
+      panelCount = 1 # re-set panelCount
+      return(panelCount)
+    } else {
+      # If this is first panel of a graph:
+      if ( ( panelCount %% (nRow*nCol) ) == 1 ) {
+        # If previous graph was open, save previous one:
+        if ( panelCount>1 & !is.null(saveName) ) {
+          saveGraph( file=paste0(saveName,(panelCount%/%(nRow*nCol))), 
+                     type=saveType)
+        }
+        # Open new graph
+        openGraph(width=nCol*7.0/3,height=nRow*2.0)
+        layout( matrix( 1:(nRow*nCol) , nrow=nRow, byrow=TRUE ) )
+        par( mar=c(4,4,2.5,0.5) , mgp=c(2.5,0.7,0) )
+      }
+      # Increment and return panel count:
+      panelCount = panelCount+1
+      return(panelCount)
+    }
+  }
+  
+  # Original scale:
+  panelCount = 1
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+  histInfo = plotPost( beta0 , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(beta[0]) , main="Intercept", compVal = as.numeric(compVal["beta0"] ))
+  for ( bIdx in 1:ncol(beta) ) {
+    panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+    if (!is.na(compVal[paste0("beta[",bIdx,"]")])){
+      histInfo = plotPost( beta[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                           xlab=bquote(beta[.(bIdx)]) , main=xName[bIdx],
+                           compVal = as.numeric(compVal[paste0("beta[",bIdx,"]")]))
+    } else{
+      histInfo = plotPost( beta[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                           xlab=bquote(beta[.(bIdx)]) , main=xName[bIdx])
+    }
+  }
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+  histInfo = plotPost( Rsq , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(R^2) , main=paste("Prop Var Accntd") , finished=TRUE )
+  
+  panelCount = 1
+  if ( pred){
+    
+    for ( pIdx in 1:ncol(pred) ) {
+      panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMarg") )
+      histInfo = plotPost( pred[,pIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+                           xlab=bquote(pred[.(pIdx)]) , main=paste0("Prediction ",pIdx) ) 
+    }
+  }# Added by Demirhan
+  # Standardized scale:
+  panelCount = 1
+  # panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  # histInfo = plotPost( zbeta0 , cex.lab = 1.75 , showCurve=showCurve ,
+  #                      xlab=bquote(z*beta[0]) , main="Intercept" )
+  # for ( bIdx in 1:ncol(beta) ) {
+  #   panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  #   histInfo = plotPost( zbeta[,bIdx] , cex.lab = 1.75 , showCurve=showCurve ,
+  #                        xlab=bquote(z*beta[.(bIdx)]) , main=xName[bIdx] )
+  # }
+  panelCount = decideOpenGraph( panelCount , saveName=paste0(saveName,"PostMargZ") )
+  histInfo = plotPost( Rsq , cex.lab = 1.75 , showCurve=showCurve ,
+                       xlab=bquote(R^2) , main=paste("Prop Var Accntd") )
+  panelCount = decideOpenGraph( panelCount , finished=TRUE , saveName=paste0(saveName,"PostMargZ") )
+  
+  #-----------------------------------------------------------------------------
+}
+
+#===============PRELIMINARY FUNCTIONS FOR POSTERIOR INFERENCES====================
+
